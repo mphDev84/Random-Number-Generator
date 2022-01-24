b74bd24df8db513182d86e839cd3c14c71a49e84
Matt <91234966+mphDev84@users.noreply.github.com>
2022-01-24 14:34:32 -0500

    Update Rand.js
    changed if/else block in the .js file that assigned a random font to the 'num' variable.

--- Rand.js
+++ Rand.js
@@ -3,17 +3,15 @@ document.querySelector("#submitButton").addEventListener("click", randNum);
 
 function randNum(min, max) {
 
-    var min =  Number(document.querySelector("#lower-number").value);
+var min =  Number(document.querySelector("#lower-number").value);
 var max =  Number(document.querySelector("#upper-number").value);
-    
 var num = Math.floor(Math.random() * ((max - min + 1))+min);
     document.querySelector("#numArea").innerHTML=num;
-   var num2 =  max/2;
-   var num3 = max/3;
-
-        if (num<=num3){
+  
+//here I have set up a little if/else block that changes font-family depending on the random number selected
+        if (num%2===0){
             document.querySelector("#numArea").style.fontFamily="fantasy";
-        }else if (num<=num2&&num>num3){
+        }else if (num%3===0){
             document.querySelector("#numArea").style.fontFamily="ariel";
           } else{
         document.querySelector("#numArea").style.fontFamily="monospace";
